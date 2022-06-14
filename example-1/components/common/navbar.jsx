d099b3bcacd12c58f9123262e3d13019d9898ab3
shoaib <shoaib.malik7421@gmail.com>
2022-06-14 11:03:07 +0530

    again

--- example-1/components/common/navbar.jsx
+++ example-1/components/common/navbar.jsx
@@ -1,43 +1,50 @@
 
 import style from '../../styles/common.module.css'
 import Link from 'next/link'
-
+import logo from '../../img/logo.png'
+import Image from 'next/image'
 
 
 
 
 export function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-light py-3 pb-4">
+        <nav className="navbar navbar-expand-lg bg-light">
             <div className="container">
                 <Link href="/">
                     <a className="navbar-brand" >
-                        Navbar
+                        <Image
+                            src={logo.src}
+                            height={'48px'}
+                            width={'68px'}
+                        />
                     </a>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse text-sans" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto ms-4 mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link text-dark me-5" aria-current="page" href="#">Category</a>
+                            <a className="nav-link text-dark me-3 rounded-5 border" aria-current="page" href="#">
+                                <i class="fa-brands fa-apple fa-xl me-2 ms-1"></i>
+                                App Store
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-dark me-5" href="#">Pricing</a>
+                            <a className="nav-link text-dark me-5 border rounded-5" href="#">
+                                <i class="fa-brands fa-google-play fa-lg me-2 ms-1"></i>
+                                Play Store
+                            </a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-dark me-5">About us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-dark me-5">List yourself</a>
-                        </li>
+                        
                     </ul>
                 </div>
                 <div className='d-flex justify-content-end'>
                     <a href="#" className='me-4 py-2 text-sans'>Sign in</a>
+                    <a href="#" className='me-4 py-2 text-sans'>Sign Up</a>
                     <button className='btn btn-orange text-sans'>
-                        Sign up
+                        List Your Business
                     </button>
                 </div>
             </div>
